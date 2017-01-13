4fae5adee077f9b5f57c7430910a55ca6759a721
OEA512 <oea@inorbit.com>
2017-01-13 22:06:07 +0000

    Source Code

---
+++ CipherLinker.h
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CipherLinker.h
+ * Author: oli
+ *
+ * Created on 08 December 2016, 18:44
+ */
+
+#ifndef CIPHERLINKER_H
+#define CIPHERLINKER_H
+#include "Buffer.h"
+#include "Cipher.h"
+#include <iostream>
+namespace CipherLinker// daat is pre padded, leaves with padding, must go throug removePadding, this does not asccount for CBC mode
+{
+    int blocksize  = 8;
+     void OperateOnBlock(unsigned char* data, int length)
+    {
+        for(int n = 0; n<(length/blocksize); n++)
+        {
+        unsigned char* block = new unsigned char[blocksize];
+        Buffer::getData(block,data,(n*blocksize), blocksize);
+        //new header file
+       //;
+        Buffer::putData(data, (n*blocksize), block, blocksize);
+        }
+    }
+     
+     
+    
+    void E(unsigned char* data,unsigned char* key,  int length)
+    {
+        //std::cout<<length<<" "<<blocksize;
+        for(int n = 0; n<(length/blocksize); n++)
+        {
+             std::cout<<"working";
+        unsigned char* block = new unsigned char[blocksize];
+        Buffer::getData(block,data,(n*blocksize), blocksize);
+
+       // Cipher::EncryptBlock(block)
+        R1::R1(block,key);// sort this shit out
+       //;
+        Buffer::putData(data, (n*blocksize), block, blocksize);
+        delete[] block;
+
+        }
+        
+        
+        
+        
+    }
+    
+    void D(unsigned char* data, int length)
+    {
+        for(int n = 0; n<(length/blocksize); n++)
+        {
+        unsigned char* block = new unsigned char[blocksize];
+        Buffer::getData(block,data,(n*blocksize), blocksize);
+       // Cipher::DecryptBlock(block)
+       //;
+        Buffer::putData(data, (n*blocksize), block, blocksize);
+        }
+    }
+    
+    
+    
+    
+}
+
+
+#endif /* CIPHERLINKER_H */
+
---
+++ Padding.h
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Padding.h
+ * Author: oli
+ *
+ * Created on 03 December 2016, 15:20
+ */
+
+#ifndef PADDING_H
+#define PADDING_H
+#include "Buffer.h"
+
+namespace Pad
+{
+   
+    //array of unsigned chars, std::vector is ok
+    //
+    int blocksize = 8;
+    
+    int getPadSize(int len)
+    {
+        int p;
+   
+        if(len<blocksize)
+        {
+           p = blocksize-len; 
+        }
+        else if(len%blocksize==0)
+        {
+            p = blocksize;
+        }
+        else
+        {
+            
+            p = blocksize-(len%blocksize);
+            
+        }
+        return p;
+        
+        
+    }
+    long getPadSize(long len)
+    {
+        long p;
+   
+        if(len<blocksize)
+        {
+           p = blocksize-len; 
+        }
+        else if(len%blocksize==0)
+        {
+            p = blocksize;
+        }
+        else
+        {
+            
+            p = blocksize-(len%blocksize);
+            
+        }
+        return p;
+        
+        
+    }
+    void padBuffer(unsigned char *data, int padsize, int length)//length is not actual size, just non null bytes
+    {
+        unsigned char c = padsize;
+        Buffer::insertByte(data,c,length,padsize );
+    
+      
+        
+    }
+    int getPrePadSize(unsigned char *data, int length)
+    {
+        
+         int padsize = data[length-1];
+         return length-padsize;
+        
+    }
+    unsigned char* removePadding(unsigned char *data, int length)// length will be a binary number as output is padded
+    {
+        int padsize = data[length-1];
+    
+        unsigned char* rem = new unsigned char[length-padsize];
+        Buffer::putData(rem,0,data,length-padsize);
+        return rem;
+        
+    }
+    void padBuffer(unsigned char *data, unsigned char *padding, int padsize, int length)
+    {
+        unsigned char c = padsize;
+        //Buffer::putData(ch
+        //char* des, int index, char* loc, int amount)
+        Buffer::putData(data,length, padding,padsize-1);
+        length = length+padsize;
+        Buffer::insertByte(data, c,length, 1);
+    }
+    
+    
+    
+    
+    
+};
+
+
+#endif /* PADDING_H */
+
